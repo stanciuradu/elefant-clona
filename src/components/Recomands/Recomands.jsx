024b57c0461fc3f3cc1f8ffc71e9ec441120af1d
radustanciu <radustanciu841@yahoo.ro>
2022-01-02 23:33:05 +0200

    Add font for heading from Google fonts

--- src/components/Recomands/Recomands.jsx
+++ src/components/Recomands/Recomands.jsx
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import recomands from '../../recomands.json';
-import RecomandsList from '../RecomandsList/RecomandsList';
+import React, { Component } from "react";
+import recomands from "../../recomands.json";
+import RecomandsList from "../RecomandsList/RecomandsList";
+import "../Recomands/Recomands.css";
 
 export class Recomands extends Component {
-    constructor(){
-        super();
-        this.state={
-            // prima data array-ul este un obiect gol
-            recomands:[]
-        };
-    }
-    componentDidMount(){
-        this.setState({recomands:recomands})
-    }
-    render() {
-        return (
-            <div className='container'>
-                <h1>Recomandări pentru tine</h1>
-                <RecomandsList recomands={this.state.recomands} />
-                <h1>Oferte speciale</h1>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      // prima data array-ul este un obiect gol
+      recomands: [],
+    };
+  }
+  componentDidMount() {
+    this.setState({ recomands: recomands });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1 className="heading">Recomandări pentru tine</h1>
+        <RecomandsList recomands={this.state.recomands} />
+        <h1 className="heading">Oferte speciale</h1>
+      </div>
+    );
+  }
 }
 
 export default Recomands;
--- src/components/RecomandsItem/RecomandsItem.jsx
+++ src/components/RecomandsItem/RecomandsItem.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import '../RecomandsItem/RecomandsItem.css';
+import React from "react";
+import "../RecomandsItem/RecomandsItem.css";
 
 function RecomandsItem(props) {
-    const {image, name, description, RRP, price,priceTop}=props;
-    return (
-        <div className='col-3'>
-            <img src={image} alt="" />
-            <h2>{name}</h2>
-            <p className='description'>{description}</p>
-            <p>RRP:{RRP}</p>
-            <p className='price'>{price}</p>
-            <p className='price-top'>{priceTop}</p>
-        </div>
-    )
+  const { image, name, description, RRP, price, priceTop } = props;
+  return (
+    <div className="col-3">
+      <img src={image} alt="" />
+      <h2>{name}</h2>
+      <p className="description">{description}</p>
+      <p>RRP:{RRP}</p>
+      <p className="price">{price}</p>
+      <p className="price-top">{priceTop}</p>
+    </div>
+  );
 }
 
 export default RecomandsItem;
